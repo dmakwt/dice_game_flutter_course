26d2e3c76303bb5253b9f293f99ffb3b4d32877e
Dhari <dmakwt@gmail.com>
2021-12-21 10:21:59 -0600

    First commit

---
+++ android/app/src/main/kotlin/com/example/dice_game_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dice_game_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int num1 = 1;
+
+  int num2 = 1;
+
+  Widget _buildPlayButton() {
+    return Container(
+      height: 40,
+      width: 100,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.red),
+        ),
+        child: Text(
+          'Play',
+          style: TextStyle(fontSize: 25),
+        ),
+        onPressed: () {
+          num1 = Random().nextInt(6) + 1;
+          num2 = Random().nextInt(6) + 1;
+
+          print(num1);
+          print(num2);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dice game'),
+          backgroundColor: Colors.red,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/dice_$num1.png',
+                    width: 130,
+                    height: 130,
+                  ),
+                  Container(
+                    width: 10,
+                  ),
+                  Image.asset(
+                    'assets/dice_$num2.png',
+                    width: 130,
+                    height: 130,
+                  ),
+                ],
+              ),
+              Container(
+                height: 10,
+              ),
+              _buildPlayButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
